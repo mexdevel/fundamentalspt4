4d52bf4938497d40c4dc5c5d7efa014286f0d67b
mexdevel <mexdevel@gmail.com>
2019-03-12 02:30:42 -0600

    fourth commit ex

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
 
+var removeFromArray = function(...args) {
+  const array = args[0]
+  return array.filter(val => !args.includes(val))
 }
+
 
 module.exports = removeFromArray
