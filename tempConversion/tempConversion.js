919b774bbf54a92d5d436419e624b1e0388a415f
mexdevel <mexdevel@gmail.com>
2019-03-12 02:42:49 -0600

    seventh commit ex

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-const ftoc = function() {
-
-}
-
-const ctof = function() {
-
-}
-
-module.exports = {
-  ftoc,
-  ctof
-}
+var ftoc = function(f) {
+  return Math.round((f - 32) * (5/9) * 10) / 10
+ }
+ 
+ var ctof = function(c) {
+   return Math.round(((c * 9/5) + 32) * 10) / 10 
+ }
+ 
+ module.exports = {
+   ftoc,
+   ctof
+ }
